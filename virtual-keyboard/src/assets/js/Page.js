afd91f1726ea72f293ea8e2ec585b33a256dae15
zhadan93 <zhadan.evgenya@yandex.ru>
2022-05-09 13:38:58 +0300

    feat: add output result to textarea

--- virtual-keyboard/src/assets/js/Page.js
+++ virtual-keyboard/src/assets/js/Page.js
@@ -7,7 +7,6 @@ export default class Page {
     this.textarea = null;
     this.elements = null;
     this.pressKey = null;
-    this.pointerPosition = 0;
   }
 
   renderPage() {
@@ -39,7 +38,7 @@ export default class Page {
   }
 
   handleKeydownEvent = (event) => {
-    const { code } = event;
+    const { code, key } = event;
 
     this.pressKey = this.elements[code];
 
@@ -47,6 +46,8 @@ export default class Page {
       event.preventDefault();
       this.textarea.focus();
       this.pressKey.classList.add('active');
+
+      this.outputResultToTextarea(code, key);
     }
   };
 
@@ -54,5 +55,75 @@ export default class Page {
     if (this.pressKey) {
       this.pressKey.classList.remove('active');
     }
+  };
+
+  outputResultToTextarea = (eventCode, eventKey) => {
+    const { value, selectionStart } = this.textarea;
+
+    const pointerPosition = selectionStart;
+    const textToLeftOfPointer = value.slice(0, pointerPosition);
+    const textToRightOfPointer = value.slice(pointerPosition);
+    const reg = eventCode.match(/(Key[A-Za-z])|(Digit[0-9])|[a-z]*quote|Minus|Equal|Bracket[a-z]*|[a-z]*slash|Semicolon|Comma|Period|Space/i);
+
+    switch (eventCode) {
+      case 'Backspace':
+        this.textarea.value = `${textToLeftOfPointer.slice(0, -1)}${textToRightOfPointer}`;
+        break;
+
+      case 'Tab':
+        this.textarea.value = `${textToLeftOfPointer}\t${textToRightOfPointer}`;
+        break;
+
+      case 'Enter':
+        this.textarea.value = `${textToLeftOfPointer}\n${textToRightOfPointer}`;
+        break;
+
+      case 'Delete':
+        this.textarea.value = `${textToLeftOfPointer}${textToRightOfPointer.slice(1)}`;
+        break;
+
+      case (reg ? reg[0] : null):
+        this.textarea.value = `${textToLeftOfPointer}${eventKey}${textToRightOfPointer}`;
+        break;
+
+      default:
+        this.textarea.value = `${textToLeftOfPointer}${textToRightOfPointer}`;
+        break;
+    }
+
+    this.movePointer(eventCode, pointerPosition);
+  };
+
+  movePointer = (eventCode, pointerPosition) => {
+    let position = pointerPosition;
+
+    switch (eventCode) {
+      case 'Backspace':
+      case 'ArrowLeft':
+        position -= position > 0 ? 1 : 0;
+        break;
+
+      case 'ArrowUp':
+        console.log();
+        break;
+
+      case 'ArrowDown':
+        console.log();
+        break;
+
+      case 'Delete':
+        position += 0;
+        break;
+
+      case 'Tab':
+      case 'Enter':
+      case 'ArrowRight':
+      case 'Space':
+      default:
+        position += 1;
+        break;
+    }
+
+    this.textarea.setSelectionRange(position, position);
   };
 }
